7c588c066841474b9a06ef1d237b3f9b942de49a
MentalCollatz <MentalCollatz@users.noreply.github.com>
2019-03-30 19:44:14 -0700

    fix pre-fork error code

--- src/pool/solo/rpc.py
+++ src/pool/solo/rpc.py
@@ -28,11 +28,11 @@ def json_request(method, *params):
     headers = {"Content-Type": "application/json", "Authorization": config.get("rpc_auth")}
     response = requests.post(config.get("rpc_url"), headers=headers, json=jdata)
 
-    if response.status_code != requests.codes.ok:
-        raise RpcError(code=response.status_code, message="HTTP status code")
     try:
         data = response.json()
     except ValueError as e:
+        if response.status_code != requests.codes.ok:
+            raise RpcError(code=response.status_code, message="HTTP status code")
         raise RpcError(code=500, message=str(e))
 
     if data["error"] is None:
